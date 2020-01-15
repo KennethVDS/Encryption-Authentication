f1717c0685b70b12888c4a10c9b0832aa24f2599
Kenneth Van Der Strieckt <49682872+KennethVDS@users.noreply.github.com>
2020-01-15 18:58:40 +0100

    restructuring

--- model/authMeModel.php
+++ model/authMeModel.php
@@ -1,56 +1,58 @@
 <?php
 session_start();
- 
+
 if (!isset($_GET['code'])) {
-    // Check if we need to show the "Sign In" link
-    $params = array (
-      'audience' => 'gw6X9X23Tk',
-      'scope' => 'openid',
+    $params = array(
+      'aud' => 'https://e2emerchant.itsme.be/oidc/authorization',
+      'scope' => 'openid service:gw6X9X23Tk profile email',
+      'redirect_uri' => 'https://adveva.be/index',
       'response_type' => 'code',
-      'client_id' => 'monkeyproof',
-      'state' => 'SomeRandomString',
-      'redirect_uri' => 'https://adveva.be/login'
+      'client_id' => 'MONKYPROOF',
+      'acr_values' => 'tag:sixdots.be,2016-06:acr_advanced',
+      'iss' => 'MONKYPROOF',
+      'nonce' => '6f05ad622a3d32a5a81aee5d73a5826adb8cbf63',
+      'state' => '6f05ad622a3d32a5a81aee5d73a5826adb8cbf64'
     );
  
-    $_SESSION['oauth2state']=$params['state'];
+    $_SESSION['openid']=$params['state'];
     $str_params = '';
-    foreach($params as $key=>$value) {
-      $str_params .= $key . "=" . urlencode($value) . "&";
+    foreach ($params as $key=>$value) {
+        $str_params .= $key . "=" . urlencode($value) . "&";
     }
-    ?>
- 
-    <a href="https://{AUTH0_DOMAIN}/authorize?<?php echo $str_params;?>">
-      Sign In
-    </a>
-<?php
-} elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+} elseif (empty($_GET['state']) || (isset($_SESSION['openid']) && $_GET['state'] !== $_SESSION['openid'])) {
     // If the "state" var is present in the $_GET, let's validate it
-    if (isset($_SESSION['oauth2state'])) {
-        unset($_SESSION['oauth2state']);
+    if (isset($_SESSION['openid'])) {
+        unset($_SESSION['openid']);
     }
      
     exit('Invalid state');
- 
-} elseif(isset($_GET['code']) && !empty($_GET['code'])) {
+} elseif (isset($_GET['code']) && !empty($_GET['code'])) {
     // If the auth "code" is present in the $_GET
     // let's exchange it for the access token
-    $params = array (
+    $params = array(
+
       'grant_type' => 'authorization_code',
-      'client_id' => 'monkeyproof',
-      'client_secret' => 'gw6X9X23Tk',
       'code' => $_GET['code'],
-      'redirect_uri' => 'https://adveva.be/login'
+      'redirect_uri' => 'https://adveva.be/index',
+      'client_assertion' => [
+        'iss' => 'MONKYPROOF',
+        'sub' => 'MONKYPROOF',
+        'aud' => 'https://e2emerchant.itsme.be/oidc/token',
+        'jti' => '6f05ad622a3d32a5a81aee5d73a5826adb8cbf65',
+        'exp' => '3600'
+      ],
+      'client_assertion_type' => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
     );
  
     $str_params = '';
-    foreach($params as $key=>$value) {
-      $str_params .= $key . "=" . urlencode($value) . "&";
+    foreach ($params as $key=>$value) {
+        $str_params .= $key . "=" . urlencode($value) . "&";
     }
  
     $curl = curl_init();
  
     curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://{AUTH0_DOMAIN}/oauth/token",
+      CURLOPT_URL => "https://e2emerchant.itsme.be/oidc/token",
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_CUSTOMREQUEST => "POST",
       CURLOPT_POSTFIELDS => $str_params
@@ -62,16 +64,16 @@ if (!isset($_GET['code'])) {
     curl_close($curl);
  
     if ($curl_error) {
-      echo "Error in the CURL response:" . $curl_error;
+        echo "Error in the CURL response:" . $curl_error;
     } else {
-      $arr_json_data = json_decode($curl_response);
+        $arr_json_data = json_decode($curl_response);
  
-      if (isset($arr_json_data->access_token)) {
-        $access_token = $arr_json_data->access_token;
-        $curl = curl_init();
+        if (isset($arr_json_data->access_token)) {
+            $access_token = $arr_json_data->access_token;
+            $curl = curl_init();
  
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "http://{YOUR_API_DOMAIN}/demo_api_server.php",
+            curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://e2emerchant.itsme.be/oidc/.well-known/openid-configuration",
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_CUSTOMREQUEST => "GET",
           CURLOPT_HTTPHEADER => array(
@@ -79,18 +81,18 @@ if (!isset($_GET['code'])) {
           )
         ));
  
-        $curl_response = curl_exec($curl);
-        $curl_error = curl_error($curl);
+            $curl_response = curl_exec($curl);
+            $curl_error = curl_error($curl);
  
-        curl_close($curl);
+            curl_close($curl);
  
-        if ($curl_error) {
-          echo "Error in the CURL response from DEMO API:" . $curl_error;
+            if ($curl_error) {
+                echo "Error in the CURL response from It's Me API:" . $curl_error;
+            } else {
+                echo "It's Me Response:" . $curl_response;
+            }
         } else {
-          echo "Demo API Response:" . $curl_response;
+            echo 'Invalid response, no access token was found.';
         }
-      } else {
-        echo 'Invalid response, no access token was found.';
-      }
     }
 }

84171db45176b8702cfa2f24303be4af31e9dbdf
Kenneth Van Der Strieckt <49682872+KennethVDS@users.noreply.github.com>
2020-01-24 11:50:47 +0100

    delete

--- lib/key.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_POST["key"]= "58703273357638792F423F4528472B4B6250655368566D597133743677397A24";
-$randomkey = openssl_random_pseudo_bytes ('128');
-//echo 'Your personal key =' . $randomkey;
